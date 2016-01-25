788ef03253e654e6f03016145bcf5ebcc86710f5
C-Das <chinmay98@gmail.com>
2016-01-25 13:10:43 -0500

    Added letter.js and word.js files

---
+++ word.js
@@ -0,0 +1,14 @@
+var letter = require("./letter.js");
+
+function Word(wrd){
+    this.charac = let;
+    this.appear = false;
+
+    this.letterRender = function(charac){
+      if(this.charac === false ){
+        return "_";
+      }else {
+        return charac;
+      }
+    }
+}
